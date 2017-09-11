1c08db31e3b1c86e390794d0604ba9fbbc79dc38
Claire Johnson <c.eliseee@gmail.com>
2017-09-11 11:34:44 -0700

    Added all templates, working on app component

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,28 @@
 angular.module('video-player')
 
 .component('app', {
-  // TODO
+
+  templateUrl: 'src/templates/app.html',
+
+  videos: window.exampleVideoData,
+
+  // currentVideo: window.exampleVideoData[0],
+
+  controller: function() {
+    const selectVideo = () => {
+
+    };
+
+    const searchResults = () => {
+
+    };
+
+    this.selectVideo = selectVideo;
+    this.searchResults = searchResults;
+  }
 });
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   angular.bootstrap(document, ['video-player']);
+// });
