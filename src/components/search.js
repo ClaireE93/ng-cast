6acf465c9c5b11a0f76e25766eea351ec6ad2cbc
Hack Reactor Student <student@hackreactor.com>
2017-09-11 18:57:23 -0700

    Bad commit

--- src/components/search.js
+++ src/components/search.js
@@ -1,5 +1,17 @@
 angular.module('video-player')
 
 .component('search', {
+
+  bindings: {
+    // searchResults: '<',
+  },
+
   templateUrl: 'src/templates/search.html',
+
+  // controller: function(youTube) {
+  //   this.result = (value) => {
+  //     console.log('searching')
+  //     youTube.search(value, this.searchResults);
+  //   };
+  // },
 });
